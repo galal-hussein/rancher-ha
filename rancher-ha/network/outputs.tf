f9195c19188eb5098042049551b364c5d5785f96
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2017-08-23 03:56:34 +0200

    Fix typo in network output

--- rancher-ha/network/outputs.tf
+++ rancher-ha/network/outputs.tf
@@ -8,6 +8,5 @@ output "elb_sg_id" {
 
 output "management_node_sgs" {
   value = "${join(",", list(aws_security_group.management_allow_elb.id,
-            aws_security_group.management_allow_internal.id,
-            module.bastion_sgs.bastion_id)))}"
+            aws_security_group.management_allow_internal.id, module.bastion_sgs.bastion_id))}"
 }
