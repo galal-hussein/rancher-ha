6273a22fc39a95fdbf58d2de9a8913c98d68acad
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2017-08-23 02:58:08 +0200

    Revert to manual sg creation

--- rancher-ha/network/main.tf
+++ rancher-ha/network/main.tf
@@ -15,12 +15,127 @@ resource "aws_iam_server_certificate" "rancher_com" {
   }
 }
 
-module "management_sgs" {
-  source = "../../modules/aws/network/security_groups/mgmt/ha"
+// https://github.com/rancher/terraform-modules/pull/13
+//module "management_sgs" {
+//  source = "../../modules/aws/network/security_groups/mgmt/ha"
 
-  vpc_id               = "${var.aws_vpc_id}"
-  private_subnet_cidrs = "${var.aws_subnet_cidrs}"
+//  vpc_id               = "${var.aws_vpc_id}"
+//  private_subnet_cidrs = "${var.aws_subnet_cidrs}"
+//}
+
+variable "vpc_id" {}
+
+variable "private_subnet_cidrs" {}
+
+resource "aws_security_group" "management_elb" {
+  name        = "${var.aws_env_name}-management_elb_sg"
+  description = "Allow ports rancher "
+  vpc_id      = "${var.aws_vpc_id}"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
+
+resource "aws_security_group" "management_allow_elb" {
+  name        = "${var.aws_env_name}-rancher_ha_allow_elb"
+  description = "Allow Connection from elb"
+  vpc_id      = "${var.aws_vpc_id}"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = ["${aws_security_group.management_elb.id}"]
+  }
+
+  ingress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    security_groups = ["${aws_security_group.management_elb.id}"]
+  }
+
+  ingress {
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = ["${aws_security_group.management_elb.id}"]
+  }
+}
+
+resource "aws_security_group" "management_allow_internal" {
+  name        = "rancher_ha_allow_internal"
+  description = "Allow Connection from internal"
+  vpc_id      = "${var.vpc_id}"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${split(",", var.aws_subnet_cidrs)}"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["${split(",", var.aws_subnet_cidrs)}"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["${split(",", var.aws_subnet_cidrs)}"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["${split(",", var.aws_subnet_cidrs)}"]
+  }
+
+  ingress {
+    from_port   = 9345
+    to_port     = 9345
+    protocol    = "tcp"
+    cidr_blocks = ["${split(",", var.aws_subnet_cidrs)}"]
+  }
+}
+
 
 module "bastion_sgs" {
   source = "../../modules/aws/network/security_groups/bastion"
--- rancher-ha/network/outputs.tf
+++ rancher-ha/network/outputs.tf
@@ -2,11 +2,11 @@ output "rancher_com_arn" {
   value = "${aws_iam_server_certificate.rancher_com.arn}"
 }
 
-output "management_node_sgs" {
-  value = "${join(",", list(module.management_sgs.management_node_sgs,
-            module.bastion_sgs.bastion_id))}"
+output "elb_sg_id" {
+  value = "${aws_security_group.management_elb.id}"
 }
 
-output "elb_sg_id" {
-  value = "${module.management_sgs.elb_sg_id}"
+output "management_node_sgs" {
+  value = "${join(",", list(aws_security_group.management_allow_elb.id,
+            aws_security_group.management_allow_internal.id))}"
 }
