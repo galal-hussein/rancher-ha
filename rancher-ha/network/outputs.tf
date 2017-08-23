4fd71800e0c20319fe63d438bed16751bbfc6124
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2017-08-23 03:43:25 +0200

    Add Jenkins destroy and fix bastion bug

--- rancher-ha/network/outputs.tf
+++ rancher-ha/network/outputs.tf
@@ -8,5 +8,6 @@ output "elb_sg_id" {
 
 output "management_node_sgs" {
   value = "${join(",", list(aws_security_group.management_allow_elb.id,
-            aws_security_group.management_allow_internal.id))}"
+            aws_security_group.management_allow_internal.id,
+            module.bastion_sgs.bastion_id)))}"
 }
