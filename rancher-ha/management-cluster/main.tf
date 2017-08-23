861db4456b7a7b148821cf2919bd5d8e8eded542
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2017-08-23 06:04:20 +0200

    decrease ttl

--- rancher-ha/management-cluster/main.tf
+++ rancher-ha/management-cluster/main.tf
@@ -78,6 +78,6 @@ resource "aws_route53_record" "www" {
    zone_id = "${var.zone_id}"
    name = "${var.domain_name}"
    type = "CNAME"
-   ttl = "300"
+   ttl = "30"
    records = ["${module.management_elb.dns_name}"]
 }
