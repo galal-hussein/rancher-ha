085f82c8e5e2ef4eff59534b1d290f20b49de1fa
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2017-08-27 03:14:35 +0200

    Fix vpc and subnets vars in database module

--- rancher-ha/database/variables.tf
+++ rancher-ha/database/variables.tf
@@ -4,12 +4,6 @@ variable "aws_access_key" {}
 
 variable "aws_region" {}
 
-variable "aws_vpc_id" {}
-
-variable "aws_subnet_cidrs" {}
-
-variable "aws_subnet_ids" {}
-
 variable "aws_s3_bucket" {}
 
 variable "aws_env_name" {}
